7ca8f7720faa5d6380209f3e80b999e146a8fe2a
Craig Joly <craig@snowkeep.ca>
2025-11-05 21:17:17 -0500

    add artaey's 3D doomcoin and the Heresy theme

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,11 @@ Hooks.once('diceSoNiceReady', dice3d => {
   dice3d.addSystem({
     id: "graveDiggers", name: "Doomsong Gravediggers",
   }, "preferred");
-
+  
+  dice3d.addSystem({
+    id: "heresy", name: "Doomsong Heresy",
+  }, "preferred");
+  
   dice3d.addColorset({
     name: 'dgd',
     category: 'Doomsong',
@@ -14,14 +18,15 @@ Hooks.once('diceSoNiceReady', dice3d => {
     material: 'metal',
   });
 
-  dice3d.addDicePreset({
-    system: 'graveDiggers',
-    colorset: 'dgd',
-    type: 'd2',
-    labels: [
-      'modules/doomsong-dice/textures/lhmuu_coin/doomcoin-skull.webp',
-      'modules/doomsong-dice/textures/lhmuu_coin/doomcoin-crown.webp',
-    ]
+  dice3d.addColorset({
+    name: 'heresy',
+    category: 'Doomsong',
+    description: 'Doomsong Heresy Die and Coin',
+    foreground: '#ffffff',
+    background: '#1c1c1c',
+    outline: 'none',
+    texture: 'marble',
+    material: 'stone',
   });
 
   dice3d.addDicePreset({
@@ -36,5 +41,47 @@ Hooks.once('diceSoNiceReady', dice3d => {
       'modules/doomsong-dice/textures/gravediggers/gravediggers_5.webp',
       'modules/doomsong-dice/textures/gravediggers/gravediggers_6.webp',
     ]
+  });
+
+  dice3d.addDicePreset({
+    system: 'graveDiggers',
+    colorset: 'dgd',
+    type: 'd2',
+    modelFile: "modules/doomsong-dice/models/Doomcoin.glb",
+  });
+  
+  dice3d.addDicePreset({
+    system: 'graveDiggers',
+    colorset: 'dgd',
+    type: 'dc',
+    modelFile: "modules/doomsong-dice/models/Doomcoin.glb",
+  });
+
+  dice3d.addDicePreset({
+    system: 'heresy',
+    colorset: 'heresy',
+    type: 'd6',
+    labels: [
+      'modules/doomsong-dice/textures/heresy/heresy_1.webp',
+      'modules/doomsong-dice/textures/heresy/heresy_2.webp',
+      'modules/doomsong-dice/textures/heresy/heresy_3.webp',
+      'modules/doomsong-dice/textures/heresy/heresy_4.webp',
+      'modules/doomsong-dice/textures/heresy/heresy_5.webp',
+      'modules/doomsong-dice/textures/heresy/heresy_6.webp',
+    ]
+  });
+
+  dice3d.addDicePreset({
+    system: 'heresy',
+    colorset: 'heresy',
+    type: 'd2',
+    modelFile: "modules/doomsong-dice/models/Doomcoin.glb",
+  });
+  
+  dice3d.addDicePreset({
+    system: 'heresy',
+    colorset: 'heresy',
+    type: 'dc',
+    modelFile: "modules/doomsong-dice/models/Doomcoin.glb",
   });
 })
